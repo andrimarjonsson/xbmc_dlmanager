1ffcd5cdb470ab016f7b213e773fbdfa2b8fef4b
Fuzzwah <rob.crouch@gmail.com>
2013-02-28 14:42:52 -0700

    modified hardcoded addon dir to special://home/addons/

--- default.py
+++ default.py
@@ -8,17 +8,17 @@ class Screensaver(xbmc.Monitor) :
         def onScreensaverDeactivated(self):
 		xbmc.log(msg="DLMGR: XBMC in use",level=xbmc.LOGDEBUG)
 		os.system("sudo /etc/init.d/sickbeard stop") #stops the sickbeard service
-		os.system("/home/xbian/.xbmc/addons/script.service.dlmanager/trans_speedlimit_on.sh") #puts transmission into speed limited mode
+		os.system("special://home/addons/script.service.dlmanager/trans_speedlimit_on.sh") #puts transmission into speed limited mode
 
         def onScreensaverActivated(self):
 		xbmc.log(msg="DLMGR: XBMC in Standby",level=xbmc.LOGDEBUG)
 		os.system("sudo /etc/init.d/sickbeard start") #starts the sickbeard service
-		os.system("/home/xbian/.xbmc/addons/script.service.dlmanager/trans_speedlimit_off.sh") #disables transmission speed limited mode
+		os.system("special://home/addons/script.service.dlmanager/trans_speedlimit_off.sh") #disables transmission speed limited mode
 
         def onAbortRequested(self):
 		xbmc.log(msg="DLMGR: XBMC is closing",level=xbmc.LOGDEBUG)
 		os.system("sudo /etc/init.d/sickbeard start") #starts the sickbeard service
-		os.system("/home/xbian/.xbmc/addons/script.service.dlmanager/trans_speedlimit_off.sh") #disables transmission speed limited mode
+		os.system("special://home/addons/script.service.dlmanager/trans_speedlimit_off.sh") #disables transmission speed limited mode
 
 xbmc.log(msg="DLMGR: Working",level=xbmc.LOGDEBUG)
 
